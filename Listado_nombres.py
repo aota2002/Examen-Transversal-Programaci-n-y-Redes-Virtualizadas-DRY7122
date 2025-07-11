0ee87283c62fc92afc3e185e131480f95f0300bb
NetAcad DEVASC <changeme@example.com>
2025-07-11 21:54:02 +0000

    Item 1 agregar repositorio de nombres

---
+++ Listado_nombres.py
@@ -0,0 +1,12 @@
+def main():
+    integrantes = [
+        "Alexander Tihuel",
+        "Justin Hernandez",
+
+    ]
+    print("Integrantes del grupo:")
+    for nombre in integrantes:
+        print(f" - {nombre}")
+
+if __name__ == "__main__":
+    main()
